4e33efa1fbb7110308d45f6632eb689ace70fee6
Tathagata Das <tathagata.das1565@gmail.com>
2013-12-24 00:26:07 +0000

    Updated HdfsRecoeveryTest to print more stats for debugging missing files.

--- streaming-tests/src/main/scala/streaming/perf/HdfsRecoveryTest.scala
+++ streaming-tests/src/main/scala/streaming/perf/HdfsRecoveryTest.scala
@@ -1,6 +1,7 @@
 package streaming.perf
 
 import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.SparkContext._
 import org.apache.spark.streaming.{Milliseconds, Time}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
@@ -37,11 +38,21 @@ class HdfsRecoveryTest extends PerfTest {
     // Verify the running counts. For any key the running count should be in the sequence
     // 1, 3, 6, 10, 15, 21, ... (i.e., nth number is sum of 1..n)
     val expectedCounts = (1L to maxRecordsPerFile).map(x => (1L to x).reduce(_ + _)).toSet
+    wordStream.foreach((rdd: RDD[(String, Long)], time: Time) => {
+      val partitionCounts = rdd.sparkContext.runJob(rdd.mapPartitions(iter => 
+          iter.toSeq.groupBy(_._1).toSeq.map(x => (x._1, x._2.map(_._2).sum)).toIterator
+          ), (iter: Iterator[(String, Long)]) => iter.toArray)
+      println("New partition counts (" + partitionCounts.size + ") at " + time + " = " + 
+        partitionCounts.map(_.mkString("[", ", ", "]")).mkString("[", ", ", "]"))
+      val counts = rdd.reduceByKey(_ + _, 1).collect()
+      println("New total count at " + time + " = " + counts.mkString("[", ", ", "]"))
+    })
     runningCountStream.foreach((rdd: RDD[(String, Long)], time: Time) => {
       val counts = rdd.collect()
       val possibleCounts = expectedCounts
       val expected = counts.forall { case (word, count) => possibleCounts.contains(count) }
       println("Running counts at " + time + " = " + counts.mkString("[", ", ", "]") + (if (!expected) ", no match" else ""))
+      println("-" * 40)
     })
 
     // Run the computation
